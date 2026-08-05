270ef3ffad1c938598af2bee87d5c4c8120ca1cc
bhatraju giri <girichandrayadav143@gmail.com>
2026-08-05 18:15:26 +0530

    Add Render compatibility wrapper

---
+++ src/server/server.js
@@ -0,0 +1,3 @@
+// Compatibility wrapper for Render if the service is configured to start from src/server/server.js
+// This file forwards execution to the real server implementation in the repository root.
+require('../../server/server.js');
